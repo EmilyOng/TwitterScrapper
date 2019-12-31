d649a3568c7757ee920f0750f80a89e11cd9c927
Emily Ong <emilyohq11@gmail.com>
2019-12-31 14:12:32 +0800

    Optimized adding data when mutation is observed

--- js/content.js
+++ js/content.js
@@ -1,4 +1,34 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse){
+  function setProfile (elem) {
+    var author = elem.getAttribute("data-name"); // get tweet author
+    var username = elem.getAttribute("data-screen-name"); // get tweet handle
+    var id = elem.getAttribute("data-tweet-id"); // get tweet id
+    var text = elem.querySelectorAll(".tweet-text");
+    text = Array.from(text);
+    var original = "", quoted = "";
+    if (elem.getAttribute("data-retweet-id") == undefined) {
+      original = text[0].textContent;
+    }
+    else {
+      try {
+        original = text[0].textContent;
+        quoted = text[1].textContent
+      } catch (e) {
+
+      } finally {
+        original = "";
+        quoted = text[0].textContent;
+      }
+    }
+
+    var curr = {"original": original,
+                "quoted": quoted,
+                "date": 0,
+                "author": author,
+                "username": username,
+                "id": id};
+    return curr;
+  }
   /* Get Data */
   function getData (url) {
     var tweet = document.getElementsByClassName("tweet");
@@ -16,24 +46,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse){
       }
       console.log("Traversing tweets");
       for (var i=0; i<size; i++){
-        var author = tweet[i].getAttribute("data-name"); // get tweet author
-        var username = tweet[i].getAttribute("data-screen-name"); // get tweet handle
-        var id = tweet[i].getAttribute("data-tweet-id"); // get tweet id
-        var text = tweet[i].querySelectorAll(".tweet-text");
-        text = Array.from(text);
-        var original = text[0].textContent, quoted = "";
-        try {
-          quoted = text[1].textContent;
-        } catch (e) {
-
-        } finally {
-        }
-        var curr = {"original": original,
-                    "quoted": quoted,
-                    "date": date[i].textContent,
-                    "author": author,
-                    "username": username,
-                    "id": id};
+        var curr = setProfile(tweet[i]);
         if (!dataStorage.includes(JSON.stringify(curr))) {
           dataStorage.push(JSON.stringify(curr));
           data.push(curr);
@@ -72,7 +85,35 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse){
           if (mutationList.length > 0) {
             // Changes in the number of tweets
             console.log("Mutation detected");
-            getData(tweets_url);
+            var dataStorage = JSON.parse(result[tweets_url]);
+            var data = [];
+            for (var i=0; i<mutationList.length; i++) {
+              if (mutationList[i].type == "childList") {
+                var addedNodes = mutationList[i].addedNodes;
+                for (var j=0; j<addedNodes.length; j++) {
+                  var tweet = addedNodes[j].children[0];
+                  var curr = setProfile(tweet);
+                  dataStorage.push(JSON.stringify(curr));
+                  data.push(curr);
+                }
+              }
+            }
+            sendData = false;
+            chrome.storage.local.set({[tweets_url]: JSON.stringify(dataStorage)}, function () {
+              console.log("Updating storage");
+              sendData = true;
+            });
+            function awaitData () {
+              if (!sendData) {
+                console.log("Waiting for data");
+                setInterval(awaitData, 100);
+              }
+              else {
+                clearInterval(awaitData);
+                sendResponse(data);
+              }
+            }
+            awaitData();
           }
         }
         const observer = new MutationObserver(callback);
